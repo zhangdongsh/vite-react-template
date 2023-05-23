a594389befedc44135bd6487e149b4c480821e06
DongSheng-Zhang1 <Dongsheng.Zhang1@budweiserapac.com>
2023-05-23 17:55:22 +0800

    fix: lint-staged 增加前置ts类型校验检测

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
@@ -6,14 +6,15 @@ import viteLogo from '/vite.svg';
 function App() {
   const [count, setCount] = useState(0);
 
-  const a = 2;
-  console.log(b, b.c);
+  useEffect(() => {
+    setCount(11);
+  }, []);
 
   return (
     <>
       <div>
         <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' a=1 />
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
         <a href='https://react.dev' target='_blank'>
           <img src={reactLogo} className='logo react' alt='React logo' />
